4117fe041eb9f1757d93d1e7a088fbbee1898248
stanleyamunze03@gmail.com <stanleyamunze03@gmail.com>
2024-06-22 19:18:01 +0300

    this commit will hide sensitive information

--- src/db.config.js
+++ src/db.config.js
@@ -1,13 +1,14 @@
 import pkg from 'pg';
+import { config } from 'dotenv';
 
 const { Pool } = pkg;
+config();
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'stan',
-    port: 5432,
-    database: 'products'
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    password:process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE
 })
 export default pool;
